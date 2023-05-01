bf229bc4b427942265d608f0b3e4fb5cc244b3e6
Name-3R1C <ericfeng94@hotmail.com>
2023-04-30 21:08:11 -0700

    Add POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -33,6 +35,11 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
